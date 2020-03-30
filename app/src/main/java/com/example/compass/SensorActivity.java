e1f78cb27219686267541fa6bf6ab5fd4937372f
albin <al1100ol-s@student.lu.se>
2020-03-30 15:43:41 +0200

    Initial commit - Compass and sensor reading complete

---
+++ app/src/main/java/com/example/compass/SensorActivity.java
@@ -0,0 +1,56 @@
+package com.example.compass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.widget.TextView;
+
+public class SensorActivity extends CompassActivity implements SensorEventListener{
+
+    TextView accelerometer;
+    TextView orientation;
+
+    private String orientationString = "";
+    private float[] orientationVector = new float[3];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor);
+
+        accelerometer = findViewById(R.id.txt_accelerometer);
+        orientation = findViewById(R.id.txt_tiltDirection);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        super.onSensorChanged(event);
+
+        String valueString = "Accelerometer x: " + super.mLastAccelerometer[0] + "\n" +
+                "Accelerometer y: " + super.mLastAccelerometer[1] + "\n" +
+                "Accelerometer z: " + super.mLastAccelerometer[2];
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            accelerometer.setText(valueString);
+
+            super.mSensorManager.getOrientation(super.rMat, orientationVector);
+
+            if (orientationVector[2] > (Math.PI/18) && orientationVector[2] <= (Math.PI/2)) {
+                orientationString = "Right";
+                orientation.setText(orientationString);
+            } else if (orientationVector[2] < -(Math.PI/18) && orientationVector[2] >= -(Math.PI/2)) {
+                orientationString = "Left";
+                orientation.setText(orientationString);
+            } else if (orientationVector[2] <= (Math.PI/18) && orientationVector[2] >= -(Math.PI/18)) {
+                orientationString = "Face up";
+                orientation.setText(orientationString);
+            }
+        }
+    }
+}
